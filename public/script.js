f7dce35f22c0f067b9c1b4db7aab49bd772b4a50
email-maker <email@yatendrabulkmailng.info>
2025-12-12 09:21:09 +0530

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,31 @@
+logoutBtn.addEventListener("dblclick", () => {
+  fetch("/logout", { method:"POST" })
+  .then(()=> location.href="/");
+});
+
+sendBtn.onclick = () => {
+  sendBtn.disabled = true;
+  sendBtn.innerText = "Sending...";
+
+  fetch("/send", {
+    method:"POST",
+    headers:{ "Content-Type":"application/json" },
+    body: JSON.stringify({
+      senderName: senderName.value,
+      email: email.value,
+      password: pass.value,
+      subject: subject.value,
+      message: message.value,      
+      recipients: recipients.value  
+    })
+  })
+  .then(r=>r.json())
+  .then(d=>{
+    statusMessage.innerText = d.message;
+    alert(d.message);
+  })
+  .finally(()=>{
+    sendBtn.disabled = false;
+    sendBtn.innerText = "Send All";
+  });
+};
